b853878eb1d6a0b1afede6a7f771935963e53b8b
Camille Ollivier <camilleollivier@yahoo.fr>
2022-07-25 11:45:27 +0200

    header_update

--- backend/app.js
+++ backend/app.js
@@ -34,7 +34,7 @@ mongoose.connect(
 
 //Eviter les erreurs de CORS
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     res.setHeader('Access-Control-Allow-Credentials','true');
