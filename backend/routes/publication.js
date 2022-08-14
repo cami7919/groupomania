fd6e481d9ab22454b8a4a8e9f070157a76f4399b
Camille Ollivier <camilleollivier@yahoo.fr>
2022-08-14 13:51:05 +0200

    like_premiere_partie

--- backend/routes/publication.js
+++ backend/routes/publication.js
@@ -1,23 +1,20 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const publicationCtrl = require ('../controllers/publication');
+const publicationCtrl = require("../controllers/publication");
 // const auth = require('../middleware/auth');
-const multer = require ('../middleware/multer-config');
+const multer = require("../middleware/multer-config");
 
 //routes des publications : /api/publication
- router.post('/',   multer, publicationCtrl.createPublication);          
-router.put('/:id',  multer, publicationCtrl.modifyPublication);
-router.delete('/:id', multer, publicationCtrl.removePublication);
-router.get('/:id',  publicationCtrl.getOnePublication);
-router.get('/',  publicationCtrl.getAllPublications);
-router.post("/:id/like",  publicationCtrl.likePublication);
+router.post("/", multer, publicationCtrl.createPublication);
+router.put("/:id", multer, publicationCtrl.modifyPublication);
+router.delete("/:id", multer, publicationCtrl.removePublication);
+router.get("/:id", publicationCtrl.getOnePublication);
+router.get("/", publicationCtrl.getAllPublications);
+router.patch("/:id/like", publicationCtrl.likePublication);
 
 // commentaires
- router.patch('/comment-pub/:id', publicationCtrl.commentPub);
- router.patch('/edit-comment-pub/:id', publicationCtrl.editCommentPub);
- router.patch('/delete-comment-pub/:id', publicationCtrl.deleteCommentPub);
+router.patch("/comment-pub/:id", publicationCtrl.commentPub);
+router.patch("/edit-comment-pub/:id", publicationCtrl.editCommentPub);
+router.patch("/delete-comment-pub/:id", publicationCtrl.deleteCommentPub);
 
-module.exports = router ;
-
-
-
+module.exports = router;
